11ef313282dc24c3aa13c5e0060c4abc82f5d811
akeyi2018 <akeyi2016@gmail.com>
2020-08-30 08:15:11 +0900

    add write sunrise info to json file

--- flaskr/sunrise.py
+++ flaskr/sunrise.py
@@ -6,13 +6,13 @@ import ephem
 import json
 
 class sun_info:
-
     def __init__(self):
         self.postal_code = '252-0301'
         #日の出入り時間の前後３０分にカーテン開閉を行う
         self.adjust_time = timedelta(minutes=30)
         #緯度経度書き込み用Jsonファイル名
         self.location_json_file = 'location.json'
+        self.sun_info_json_file = 'suninfo.json'
 
     #jsonファイルから緯度経度を取得する
     def get_latlon_from_json(self):
@@ -21,15 +21,19 @@ class sun_info:
     
     #jsonに緯度経度を書き込む
     def write_latlon_to_json(self):
- 
         res = self.get_latlon()
         if res is None: return res
  
         json_data = {'lat': res[0], 'lon': res[1]}
- 
         with open(self.location_json_file, 'w') as json_file:
             json.dump(json_data, json_file, indent=4)
 
+    #jsonに日の出入り時間を書き込む
+    def write_sunrise_info_to_json(self, sunset, sunrise, status):
+        json_data = {'open': sunset, 'close': sunrise, 'status': status}
+        with open(self.sun_info_json_file, 'w') as json_file:
+            json.dump(json_data, json_file, indent=4)
+    
     #郵便番号より経度、緯度を取得する（geocoding.jp)   
     def get_latlon(self):
         try:
@@ -43,7 +47,6 @@ class sun_info:
 
     #jsonファイルより日の出入り時間を計算する（取得）
     def get_sun_info_from_json(self):
-
         location = ephem.Observer()
         res = self.get_latlon_from_json()
         if res is None:
@@ -59,12 +62,11 @@ class sun_info:
         #次の日の入りと日の出時間を取得
         sunrise_time = str(ephem.localtime(location.next_rising(sun)) + self.adjust_time)
         sunset_time = str(ephem.localtime(location.next_setting(sun)) - self.adjust_time)
-
-        return sunset_time, sunrise_time
+        #計算結果をjsonファイルに書き込む
+        self.write_sunrise_info_to_json(sunset_time, sunrise_time, 'init')
     
     #日の出入り時間を計算する（取得）
     def get_sun_info(self):
-
         location = ephem.Observer()
         res = self.get_latlon()
         if res is None:
@@ -82,8 +84,6 @@ class sun_info:
 
         return sunset_time, sunrise_time
     
-
 if __name__ == '__main__':
-
     suninfo = sun_info()
     print(suninfo.get_sun_info_from_json())
